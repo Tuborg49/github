f15726785f327acf5e8c6c38eeae4bafb75aa73d
Tuborg49 <eldhosejobyevj@gmail.com>
2026-04-03 23:44:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+$(".carousel").owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
+
+
+  function sendMail(){
+    event.preventDefault();
+
+    var params = {
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        subject:document.getElementById('subject').value,
+        message:document.getElementById('message').value,
+       
+    };
+
+    const serviceID = "service_uiogcxm";
+    const templateID = "template_p5tvzbg";
+
+
+        emailjs.send(serviceID, templateID, params)
+        .then(res=>{
+            document.getElementById('name').value="";
+            document.getElementById('email').value="";
+            document.getElementById('subject').value="";
+            document.getElementById('message').value="";
+            alert("Your message was sent successfully");
+        })
+        .catch(err=>console.log(err));
+
+  }
